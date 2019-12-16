c1c8548f493b6da19618abadb4da38a42179f5ca
focacci <michaelfocacci@gmail.com>
2019-12-16 18:26:04 -0500

    Bus sign up UI started

---
+++ SchussApp-iOS/BusSignUp.swift
@@ -0,0 +1,9 @@
+//
+//  BusSignUp.swift
+//  SchussApp-iOS
+//
+//  Created by Michael Focacci on 12/16/19.
+//  Copyright © 2019 Schussmeisters Ski Club. All rights reserved.
+//
+
+import Foundation
