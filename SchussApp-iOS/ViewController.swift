5bb005df82810bf5e27daa248447a40762f0cae3
focacci <michaelfocacci@gmail.com>
2019-12-16 00:06:41 -0500

    Initial Commit

---
+++ SchussApp-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchussApp-iOS
+//
+//  Created by Michael Focacci on 12/16/19.
+//  Copyright © 2019 Schussmeisters Ski Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
