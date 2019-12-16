8876c511b372937628700aaa601ccb0af567d279
focacci <michaelfocacci@gmail.com>
2019-12-16 18:27:27 -0500

    Bus sign up UI started

--- SchussApp-iOS/BusSignUp.swift
+++ SchussApp-iOS/BusSignUp.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2019 Schussmeisters Ski Club. All rights reserved.
 //
 
-import Foundation
+
+
+
+// store sign-up information in a class
+
+class BusSignUp {
+    
+    var first_name: String
+    var last_name: String
+    var pass_number: Int
+    
+    init(_ first_name: String, _ last_name: String, _ pass_number: Int) {
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pass_number = pass_number
+    }
+    
+    
+    func send_to_server() {
+        print("Sending to server")
+    }
+
+
+}
+
--- SchussApp-iOS/ViewController.swift
+++ SchussApp-iOS/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var first_name_test: UILabel!
+    @IBOutlet weak var last_name_test: UILabel!
+    @IBOutlet weak var pass_number_test: UILabel!
+    
+    @IBOutlet weak var first_name: UITextField!
+    @IBOutlet weak var last_name: UITextField!
+    @IBOutlet weak var pass_number: UITextField!
+    
+    @IBOutlet weak var sign_up: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        
     }
-
+    
+    //MARK: Actions
+    @IBAction func collect_information(_ sender: UIButton) {
+        first_name_test.text = first_name.text
+        last_name_test.text = last_name.text
+        pass_number_test.text = pass_number.text
+    }
+    
 
 }
-
